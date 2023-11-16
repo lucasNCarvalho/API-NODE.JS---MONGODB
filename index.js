78fa198b1182ec1a5bcaa11d7e55bcec783849a3
Lucas Nunes Carvalho <lucasnc10@hotmail.com>
2023-11-16 09:59:17 -0300

    update on port

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.get('/', (req, res) => {
 
 app.use('/person', persoRoutes);
 
+const port = process.env.PORT || 3000;
 
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
@@ -30,7 +31,7 @@ const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.bqvxgjr.mongodb.net/bancodaapi?retryWrites=true&w=majority`)
 .then(() => {
     console.log("conectamos ao MongoDB!")
-    app.listen(3000)
+    app.listen(port)
 })
 .catch((err) => console.log(err))
 
